5b6b75d80899a37d5d7d8b65ccea23043a380e38
kswelder <welderhayne2000@gmail.com>
2022-12-27 14:39:20 -0300

    Limpando classe

--- src/main/java/banco/App.java
+++ src/main/java/banco/App.java
@@ -8,7 +8,6 @@ public class App
 {
   public static void main( String[] args )
   {
-    Client novo = new Client("welder","09/07/170aa0");
-    System.out.println( "Hello World!\n"+novo.informacoes() );
+    System.out.println( "Hello World!");
   }
 }
