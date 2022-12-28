94cdd7493681ad11b40a351aac59d46662f90cc3
kswelder <welderhayne2000@gmail.com>
2022-12-27 21:40:17 -0300

    Testando banco de dados

--- src/main/java/banco/App.java
+++ src/main/java/banco/App.java
@@ -8,6 +8,13 @@ public class App
 {
   public static void main( String[] args )
   {
+    Database dt = new Database();
+    try {
+      dt.teste();
+    }
+    catch(Exception ex) {
+      System.out.println("Erro sqlite");
+    }
     System.out.println( "Hello World!");
   }
 }
