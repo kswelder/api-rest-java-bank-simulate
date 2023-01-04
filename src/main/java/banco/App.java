16318f3fe094eeb6b9d0af67976cb5a49a0b1fec
kswelder <welderhayne2000@gmail.com>
2023-01-03 22:57:36 -0300

    Adicionando comentario

--- src/main/java/banco/App.java
+++ src/main/java/banco/App.java
@@ -8,6 +8,7 @@ package banco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Notation Spring para inicializar aplicacao
 @SpringBootApplication
 public class App 
 {
