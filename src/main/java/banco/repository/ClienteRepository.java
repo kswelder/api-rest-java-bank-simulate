da0519815f53d63bb2da9831b2c6ec254e6ba08b
kswelder <welderhayne2000@gmail.com>
2023-01-03 22:59:34 -0300

    Em desenvolvimento

---
+++ src/main/java/banco/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package "banco.repository";
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends {}
