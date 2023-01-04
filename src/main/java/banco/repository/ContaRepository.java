7830489c0ee70e041efb7d6b6656f83e077e9f37
kswelder <welderhayne2000@gmail.com>
2023-01-04 01:11:52 -0300

    Auterando nome de arquivo

--- src/main/java/banco/repository/ContaRepository.java
+++ src/main/java/banco/repository/ContaRepository.java
@@ -1,8 +1,10 @@
 package "banco.repository";
 
+import banco.model.ContaModel;
+
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface ClienteRepository extends {}
+public interface ClienteRepository extends CrudRepository<ContaModel, Long>{}
