7b4709d056bd5087557f8f603455261fd95608c5
kswelder <welderhayne2000@gmail.com>
2023-01-12 00:02:09 -0300

    Adicionando DTO para cliente

---
+++ src/main/java/apibanco/dto/ClienteDTO.java
@@ -0,0 +1,41 @@
+package apibanco.dto;
+
+import apibanco.model.Cliente;
+import apibanco.model.Endereco;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteDTO implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String nascimento;
+
+    private Endereco endereco;
+    private List<Conta> conta;
+
+    public ClienteDTO(Cliente cliente){
+
+        this.id = cliente.getId();
+        this.nome = cliente.getNome();
+        this.nascimento = cliente.getNascimento();
+    }
+
+}
