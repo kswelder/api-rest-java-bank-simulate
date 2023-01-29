f60556605c5966ca8956ba3e1f7d76c137778444
kswelder <welderhayne2000@gmail.com>
2023-01-29 11:39:21 -0300

    Regra de negocio para tratamento de erros

--- src/main/java/apibanco/exception/RegraDeNegocioException.java
+++ src/main/java/apibanco/exception/RegraDeNegocioException.java
@@ -1,3 +1,14 @@
 package apibanco.exception;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+public class RegraDeNegocioException extends RuntimeException {
+    public RegraDeNegocioException(String message) {
+        super(message);
+    }
+}
