132349d51f1205159b2528fa44589364a110e6e1
kswelder <welderhayne2000@gmail.com>
2023-03-13 12:10:59 +0000

    Add new end point to find user by id

--- src/main/java/apibanco/controller/ClienteController.java
+++ src/main/java/apibanco/controller/ClienteController.java
@@ -1,7 +1,7 @@
 package apibanco.controller;
 
+import apibanco.dto.ClienteDTO;
 import apibanco.model.Cliente;
-import apibanco.records.ClienteRecord;
 import apibanco.service.ClienteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,24 +19,24 @@ public class ClienteController {
   private ClienteService clienteService;
 
   @GetMapping(path="/find/{nome}")
-  public ResponseEntity<ClienteRecord> findOne(@PathVariable("nome") String nome) {
+  public ResponseEntity<ClienteDTO> findOne(@PathVariable("nome") String nome) {
     return ResponseEntity.status(HttpStatus.OK).body(clienteService.findNome(nome));
   }
 
   @PostMapping(path="/save")
-  public ResponseEntity<ClienteRecord> savarRecord(@RequestBody Cliente cliente) {
-    ClienteRecord cr = clienteService.saveRecord(cliente);
+  public ResponseEntity<ClienteDTO> savarRecord(@RequestBody Cliente cliente) {
+    ClienteDTO dto = clienteService.saveRecord(cliente);
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(cr);
+    return ResponseEntity.status(HttpStatus.CREATED).body(dto);
   }
   @PutMapping(path="/update/{nome}")
-  public ResponseEntity<ClienteRecord> updateOne(@PathVariable("nome") String nome, @RequestBody Cliente cliente) {
-    ClienteRecord cr = clienteService.updateRecord(nome, cliente);
-    return ResponseEntity.status(HttpStatus.NO_CONTENT).body(cr);
+  public ResponseEntity<ClienteDTO> updateOne(@PathVariable("nome") String nome, @RequestBody Cliente cliente) {
+    ClienteDTO dto = clienteService.updateRecord(nome, cliente);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).body(dto);
   }
 
   @GetMapping(path="/list")
-  public ResponseEntity<List<ClienteRecord>> list() {
+  public ResponseEntity<List<ClienteDTO>> list() {
     return ResponseEntity.status(HttpStatus.ACCEPTED).body(clienteService.list());
   }
 }
