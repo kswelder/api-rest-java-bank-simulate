66023eac6c0b2957e452d9e05d53b363f5bdf9cb
kswelder <welderhayne2000@gmail.com>
2023-07-06 05:42:49 -0300

    Change name get to find

--- src/main/java/apibanco/service/TransactionService.java
+++ src/main/java/apibanco/service/TransactionService.java
@@ -6,6 +6,7 @@ import apibanco.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,12 +16,13 @@ public class TransactionService {
     @Autowired
     private AccountService accountService;
     public void saveTransaction(String username, Transaction transaction) {
-        Account account = accountService.getAccount(username);
+        Account account = accountService.findAccount(username);
         transaction.setAccount(account);
+        transaction.setCreatedAt(LocalDateTime.now());
         transactionRepository.save(transaction);
     }
     public List<Transaction> getListTransaction(String username) {
-        Account account = accountService.getAccount(username);
+        Account account = accountService.findAccount(username);
         return account.getTransactions();
     }
 }
