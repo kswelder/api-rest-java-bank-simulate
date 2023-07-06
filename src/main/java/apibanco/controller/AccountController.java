9bb69f59b91226511def16f30011db63df397cb3
kswelder <welderhayne2000@gmail.com>
2023-07-06 05:34:44 -0300

    Add methods ResponseEntity return void to make new Account

--- src/main/java/apibanco/controller/AccountController.java
+++ src/main/java/apibanco/controller/AccountController.java
@@ -1,11 +1,24 @@
 package apibanco.controller;
 
-import apibanco.service.AddressService;
+import apibanco.model.Account;
+import apibanco.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/account")
 public class AccountController {
     @Autowired
-    private AddressService addressService;
+    private AccountService accountService;
+
+    @GetMapping("/save")
+    public ResponseEntity<Void> saveAccount(@RequestBody Account account) {
+        accountService.saveAccount(account);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
 }
