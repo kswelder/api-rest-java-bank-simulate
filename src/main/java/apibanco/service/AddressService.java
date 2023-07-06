f8db4bd92dffafe179312fbda2990c0dbf44ab8d
kswelder <welderhayne2000@gmail.com>
2023-07-06 05:41:11 -0300

    Add LocalDateTime auto and make to child Account

--- src/main/java/apibanco/service/AddressService.java
+++ src/main/java/apibanco/service/AddressService.java
@@ -2,10 +2,13 @@ package apibanco.service;
 
 import apibanco.model.Account;
 import apibanco.model.Address;
+import apibanco.repository.AccountRepository;
 import apibanco.repository.AddressRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class AddressService {
@@ -15,18 +18,26 @@ public class AddressService {
   @Autowired
   private AccountService accountService;
 
-  public void save(String username,Address endereco) {
-    Account account = accountService.getAccount(username);
-    endereco.setAccount(account);
-    addressRepository.save(endereco);
+  public void save(String username,Address address) {
+    LocalDateTime localDateTime = LocalDateTime.now();
+    Account account = accountService.findAccount(username);
+    address.setAccount(account);
+    address.setCreatedAt(localDateTime);
+    address.setUpdatedAt(localDateTime);
+    account.setAddress(address);
+    accountService.saveAccount(account);
   }
   public void update(String username, Address addressUpdate) {
-    Account account = accountService.getAccount(username);
+    Account account = accountService.findAccount(username);
     Address address = account.getAddress();
     address.setUf(addressUpdate.getUf());
     address.setCity(addressUpdate.getCity());
     address.setStreet(address.getStreet());
     address.setNumber(address.getNumber());
-    addressRepository.save(address);
+    address.setUpdatedAt(LocalDateTime.now());
+    accountService.saveAccount(account);
+  }
+  public Address findAddress(String username) {
+    return accountService.findAccount(username).getAddress();
   }
 }
