ce09198bd646e8f0df0edf736e9523767f5e4a33
kswelder <welderhayne2000@gmail.com>
2023-07-06 12:47:34 -0300

    Add Get, Put and Post Mapping

--- src/main/java/apibanco/controller/AccountController.java
+++ src/main/java/apibanco/controller/AccountController.java
@@ -5,10 +5,9 @@ import apibanco.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/account")
@@ -16,9 +15,22 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
-    @GetMapping("/save")
+    @GetMapping("/list")
+    public ResponseEntity<List<Account>> listAccounts() {
+        return ResponseEntity.status(HttpStatus.OK).body(accountService.list());
+    }
+    @GetMapping("/find/{username}")
+    public ResponseEntity<Account> findAccount(@PathVariable("username") String username) {
+        return ResponseEntity.status(HttpStatus.OK).body(accountService.findAccount(username));
+    }
+    @PostMapping("/save")
     public ResponseEntity<Void> saveAccount(@RequestBody Account account) {
         accountService.saveAccount(account);
         return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+    @PutMapping("/{username}/update")
+    public ResponseEntity<Void> updateAccount(@PathVariable("username") String username, @RequestBody Account account) {
+        accountService.updateAccount(username, account);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
